1cfd7d4d7f3c13165e370c7caa4965534f80e6ea
Jorick Caberio <circuitfreakcoder@gmail.com>
2013-05-31 15:31:07 +0800

    Create Fish.cpp

---
+++ Fish.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+class Fish
+{
+    public:
+        bool FreshWaterFish;
+
+        void Swim()
+        {
+            cout<<((FreshWaterFish)?"Swims in lake":"Swims in sea")<<endl;
+        }
+};
+
+
+class Tuna: public Fish
+{
+    public:
+        Tuna()
+        {
+            FreshWaterFish = false;
+        }
+};
+
+class Carp: public Fish
+{
+    public:
+        Carp()
+        {
+            FreshWaterFish = true;
+        }
+};
+
+int main()
+{
+    Carp myLunch;
+    Tuna myDinner;
+
+    cout << "Getting my food to swim" << endl;
+
+    cout << "Lunch: ";
+
+    myLunch.Swim();
+
+    cout << "Dinner: ";
+    myDinner.Swim();
+
+    return 0;
+
+}
+
